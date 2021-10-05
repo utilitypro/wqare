9bcaee782fe114b72621f0ee2cf3fcec37119184
SOUMBOU Terrence <JW5943@engie.com>
2021-10-05 09:19:08 +0200

    fix success sms

--- src/app/patients/success/success.component.ts
+++ src/app/patients/success/success.component.ts
@@ -14,25 +14,25 @@ export class SuccessComponent implements OnInit {
 
   ngOnInit(): void {
     this.commonService.message.subscribe((res) => {
-      if(res != undefined){
-      // @ts-ignore
-      var _res = JSON.parse(res);
-      if(_res['page'] == 'success') {
-          // @ts-ignore
-          var _data = dt;
-          this.doc = _data['doc'];
-          this.date = _data['datetime'].split(' ')[0];
-          this.time = _data['datetime'].split(' ')[1];
-          // @ts-ignore
-          localStorage.setItem('successInfo', res);
-        }else if (localStorage.get('successInfo')) {
-        var data1 = JSON.parse(localStorage.get('successInfo'));
-        this.doc = data1['doc'];
-        this.date = data1['datetime'].split(' ')[0];
-        this.time = data1['datetime'].split(' ')[1];
-      }
-      }else if (res == undefined && localStorage.get('successInfo')){
-          var data = JSON.parse(localStorage.get('successInfo'));
+      if(res != undefined) {
+        // @ts-ignore
+        var _res = JSON.parse(res);
+          if (_res['page'] == 'success') {
+            // @ts-ignore
+            var _data = _res;
+            this.doc = _data['doc'];
+            this.date = _data['datetime'].split(' ')[0];
+            this.time = _data['datetime'].split(' ')[1];
+            // @ts-ignore
+            localStorage.setItem('successInfo', res);
+          } else if (localStorage.get('successInfo')) {
+            var data1 = JSON.parse(localStorage.getItem('successInfo'));
+            this.doc = data1['doc'];
+            this.date = data1['datetime'].split(' ')[0];
+            this.time = data1['datetime'].split(' ')[1];
+          }
+        } else if (res == undefined && localStorage.getItem('successInfo')) {
+          var data = JSON.parse(localStorage.getItem('successInfo'));
           this.doc = data['doc'];
           this.date = data['datetime'].split(' ')[0];
           this.time = data['datetime'].split(' ')[1];
